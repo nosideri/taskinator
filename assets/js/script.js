4c24279b5b1980d34da5e60b377a93afe9177e23
nosideri <madi.stollmeyer@gmail.com>
2020-08-29 01:28:11 -0700

    form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-buttonEl.addEventListener("click", createTaskHandler); {
+var createTaskHandler = function(event) {
+
+  event.preventDefault();
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+  listItemEl.textContent = "task-name";
   tasksToDoEl.appendChild(listItemEl);
 };
 
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  }
+formEl.addEventListener("submit", createTaskHandler); {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "task-name";
+  tasksToDoEl.appendChild(listItemEl);
+};
