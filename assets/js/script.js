e0154a51b9951a0fd909d0afae8bbbf4224a8586
nosideri <madi.stollmeyer@gmail.com>
2020-08-29 21:33:15 -0700

    connected task type input

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,12 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
 
   event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "task-name";
+  listItemEl.textContent = taskNameInput;
   tasksToDoEl.appendChild(listItemEl);
 };
 
